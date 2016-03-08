30d12475b9ec2e1ae3928bc6528316d2ff4f930f
gitrepository <vaddu_pavan@yahoo.com>
2016-03-07 21:22:33 -0500

    RTB03

--- server/server.js
+++ server/server.js
@@ -12,9 +12,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
+// CORS Support: Openup the REST services to every one
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/meanapp');
 mongoose.connection.once('open', function() {
+  
+  app.use('/', function(req, res, next){
+    res.send("Hello World");  
+  })
+  
   console.log('Listening on port 3000...');
   app.listen(3000);
 });
