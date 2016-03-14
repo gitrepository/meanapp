25a37e8cfe08734f268db12dafe61a4375590d47
gitrepository <vaddu_pavan@yahoo.com>
2016-03-14 12:09:35 -0400

    RTB16

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -23,7 +23,6 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
-      'bower_components/angular-route/angular-route.js',
       'bower_components/lodash/lodash.js',
       'bower_components/restangular/dist/restangular.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
